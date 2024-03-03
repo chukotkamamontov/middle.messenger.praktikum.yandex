f12980b7362445079c533eebca7537afa1dd9770
Sergey Kharitonov <chukotka.m27@gmail.com>
2024-03-03 08:52:57 +0300

    chore: added ts extentions for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: ['.eslintrc.{js,cjs,ts}'],
       parserOptions: {
         sourceType: 'script',
       },
