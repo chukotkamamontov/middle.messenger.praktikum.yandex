d7e348660b5ad737f21919347f6433991adca5a1
Sergey Kharitonov <chukotka.m27@gmail.com>
2024-07-22 21:02:44 +0300

    chore: removed unless components

--- src/pages/signin/signin.tmp.ts
+++
@@ -1,8 +0,0 @@
-import styles from './signin.module.scss';
-
-export const tmp = `
-  <div class=${styles.container}>
-    <h2>Авторизация</h2>
-    {{{form}}}
-  </div>
-`;
--- src/pages/signup/signup.tmp.ts
+++
@@ -1,8 +0,0 @@
-import styles from './signup.module.scss';
-
-export const tmp = `
-  <div class=${styles.container}>
-    <h2>Регистрация</h2>
-    {{{form}}}
-  </div>
-`;
