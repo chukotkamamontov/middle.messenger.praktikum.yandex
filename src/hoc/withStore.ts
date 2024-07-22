a5469d8ff9c67a45ca4a49ac2f761c415df95556
Sergey Kharitonov <chukotka.m27@gmail.com>
2024-07-22 22:23:19 +0300

    chore: ts errors fix 5

--- src/hoc/withStore.ts
+++ src/hoc/withStore.ts
@@ -2,16 +2,19 @@ import Store, { State, StoreEvents } from '../tools/store';
 import Block from '../tools/block';
 import { isEqual } from '../utils/storeUtils';
 
-export const withStore = (mapStateToProps: (state: State) => any) => <P extends Record<string, unknown>>(Component: typeof Block<P>) => class extends Component {
-  constructor(props: any) {
-    let state = mapStateToProps(Store.getState());
-    super({ ...props, ...mapStateToProps(Store.getState()) });
-    Store.on(StoreEvents.Updated, () => {
-      const propsFromState = mapStateToProps(Store.getState());
-      if (!isEqual(state, propsFromState)) {
-        this.setProps({ ...propsFromState });
-      }
-      state = propsFromState;
-    });
+export const withStore = (mapStateToProps: (state: State) => any) => (Component: Block) => {
+  console.log('[Component]: ', Component)
+  return class extends Component {
+    constructor(props: any) {
+      let state = mapStateToProps(Store.getState());
+      super({ ...props, ...mapStateToProps(Store.getState()) });
+      Store.on(StoreEvents.Updated, () => {
+        const propsFromState = mapStateToProps(Store.getState());
+        if (!isEqual(state, propsFromState)) {
+          this.setProps({ ...propsFromState });
+        }
+        state = propsFromState;
+      });
+    }
   }
-};
+}
